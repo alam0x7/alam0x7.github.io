28a8660bfa2ab05f63e97f3a733482d0f63b5f89
Alam0x <91639995+alam0x7@users.noreply.github.com>
2024-11-06 14:56:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const texts = ["Developer", "Designer", "Freelancer"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing-text span").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(() => {
+      setTimeout(type, 400); // jeda sebelum mengetik kata berikutnya
+    }, 1400); // jeda saat selesai satu kata
+  } else {
+    setTimeout(type, 100); // kecepatan mengetik per huruf
+  }
+}
+type();
